35afad796675125bfb39bf6fae33688abd777007
ConvolutedDog <yangjianchao16@nudt.edu.cn>
2024-11-16 23:52:20 +0800

    improve code readability

--- sass-split/sass-split.sh
+++ sass-split/sass-split.sh
@@ -7,6 +7,5 @@ CMD="./process_sass_dir --dir"
 find "$SEARCH_ROOT" -type d -name "sass_traces" | while read dir; do
     echo "Processing directory: $dir"
     $CMD "$dir"
-    find $dir -regextype posix-extended -regex ".*/kernel_([0-9]+)_gwarp_id_([0-9]+)\.split\.sass" -exec realpath {} \; > $dir/content.txt
     echo "Current Time: $(date)"
 done
