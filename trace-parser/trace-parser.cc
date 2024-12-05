65df1afa9665371aa7d83f31616685b29b8c76f9
ConvolutedDog <yangjianchao16@nudt.edu.cn>
2024-12-05 12:14:54 +0800

    improve code readability

--- trace-parser/trace-parser.cc
+++ trace-parser/trace-parser.cc
@@ -997,73 +997,63 @@ void trace_parser::process_single_file(FileData fileData) {
     std::vector<std::vector<std::vector<compute_instn>>>& conpute_instns;
     std::vector<std::pair<int, int>>* x;
   };
-  */    
-        int kernel_id = fileData.kernel_id;
-        int block_id = fileData.block_id;
+  */ 
+  int kernel_id = fileData.kernel_id;
+  int block_id = fileData.block_id;
 
-        std::string compute_instns_filepath = fileData.filepath;
-        const char *file_path = compute_instns_filepath.data();
+  std::string compute_instns_filepath = fileData.filepath;
+  const char *file_path = compute_instns_filepath.data();
 
-        std::vector<std::pair<int, int>>* x = fileData.x;
+  std::vector<std::pair<int, int>>* x = fileData.x;
 
-        if (!judge_format_compute_kernel_id_fast(kernel_id, block_id, x))
-          return;
+  if (!judge_format_compute_kernel_id_fast(kernel_id, block_id, x))
+    return;
 
-        FILE* file = fopen(file_path, "rb");
-        if (!file) {
-          printf("Cannot open file: %s\n", file_path);
-          abort();
-        }
-        fseek(file, 0, SEEK_END);
-        long fsize = ftell(file);
-        fseek(file, 0, SEEK_SET);
-        char* string = (char*)malloc(fsize + 1);
-        fread(string, 1, fsize, file);
-        string[fsize] = '\0';
-        fclose(file);
-
-        char* line = string;
-        char* next_line = nullptr;
-        unsigned _pc, _mask, gwarp_id;
-        char* context_end = string + fsize;
-
-        while(line < context_end) {
-          next_line = strchr(line, '\n');
-          if(next_line != nullptr) {
-            *next_line = '\0';
-          }
-
-          if(*line == '\0') {
-            line = next_line + 1;
-            continue;
-          }
-
-          char mask_str[9] = {0};
-
-          if (sscanf(line, "%x %8s %u", &_pc, mask_str, &gwarp_id) == 3) {
-            std::string _mask_str(mask_str);
-
-            if (_mask_str == "!") {
-              _mask = 0xffffffff;
-            } else {
-              _mask = static_cast<unsigned>(std::stoul(_mask_str, nullptr, 16));
-            }
-
-            _inst_trace_t* _inst_trace =
-              (*get_instncfg()->get_instn_info_vector())[std::make_pair(kernel_id - 1, _pc)];
-
-            conpute_instns[kernel_id - 1][gwarp_id].emplace_back(compute_instn(
-              kernel_id - 1, _pc, _mask, gwarp_id, _inst_trace, nullptr));
-          }
-
-          if(next_line != nullptr) {
-            line = next_line + 1;
-          } else {
-            break;
-          }
-        }
-
-        free(string);
+  FILE* file = fopen(file_path, "rb");
+  if (!file) {
+    printf("Cannot open file: %s\n", file_path);
+    abort();
+  }
+  fseek(file, 0, SEEK_END);
+  long fsize = ftell(file);
+  fseek(file, 0, SEEK_SET);
+  char* string = (char*)malloc(fsize + 1);
+  fread(string, 1, fsize, file);
+  string[fsize] = '\0';
+  fclose(file
+  char* line = string;
+  char* next_line = nullptr;
+  unsigned _pc, _mask, gwarp_id;
+  char* context_end = string + fsiz
+  while(line < context_end) {
+    next_line = strchr(line, '\n');
+    if(next_line != nullptr) {
+      *next_line = '\0';
+  
+    if(*line == '\0') {
+      line = next_line + 1;
+      continue;
+  
+    char mask_str[9] = {0
+    if (sscanf(line, "%x %8s %u", &_pc, mask_str, &gwarp_id) == 3) {
+      std::string _mask_str(mask_str
+      if (_mask_str == "!") {
+        _mask = 0xffffffff;
+      } else {
+        _mask = static_cast<unsigned>(std::stoul(_mask_str, nullptr, 16));
+    
+      _inst_trace_t* _inst_trace =
+        (*get_instncfg()->get_instn_info_vector())[std::make_pair(kernel_id - 1, _pc)
+      conpute_instns[kernel_id - 1][gwarp_id].emplace_back(compute_instn(
+        kernel_id - 1, _pc, _mask, gwarp_id, _inst_trace, nullptr));
+  
+    if(next_line != nullptr) {
+      line = next_line + 1;
+    } else {
+      break;
+    }
+  
+  free(string);
 }
 
 void trace_parser::process_compute_instns_fast(
